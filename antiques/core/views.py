4d61ffb96b2a34faf63b24ffbfa5a066577d5d87
piplofen <podkovyrov@gmail.com>
2026-05-11 15:05:43 +0300

    add core.html

--- antiques/core/views.py
+++ antiques/core/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 def index(request):
     context = {}
     context["title"] = "Антиквариат"
-    return render(request, "core/core.html", context=context)
+    return render(request, "core/core_alt.html", context=context)
