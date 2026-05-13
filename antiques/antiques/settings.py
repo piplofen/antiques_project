b2063fc9d0ef9ceeae5a8fe5d4b849b39faf26ee
piplofen <podkovyrov@gmail.com>
2026-05-13 10:01:09 +0300

    create Profile model

--- antiques/antiques/settings.py
+++ antiques/antiques/settings.py
@@ -91,3 +91,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- antiques/core/models.py
+++ antiques/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="Профиль")
+    avatar = models.ImageField(upload_to=f"avatars/{User.username}", null=True, blank=True, verbose_name="Аватар")
+
+    def __str__(self):
+        return f"Профиль {self.user.username}"
+
+    class Meta:
+        verbose_name = "Профиль"
+        verbose_name_plural = "Профили"
